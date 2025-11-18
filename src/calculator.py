69bc11cf942498183c7e2ed064c6052c20fc76c6
Simone Avellino <s.avellino02@gmail.com>
2025-11-18 18:49:43 +0100

    feat: implement basic arithmetic operations with enum support

--- src/calculator.py
+++ src/calculator.py
@@ -10,3 +10,46 @@
 #   Multiplication: 8
 #   Division: 2
 #
+import enum
+
+class Operation(enum.Enum):
+    SUM = 1
+    DIFFERENCE = 2
+    MULTIPLICATION = 3
+    DIVISION = 4
+
+def sum(a, b):
+    return a + b
+
+def difference(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+    
+def operation(a, b, op: Operation):
+    if op == Operation.SUM:
+        return sum(a, b)
+    elif op == Operation.DIFFERENCE:
+        return difference(a, b)
+    elif op == Operation.MULTIPLICATION:
+        return multiplication(a, b)
+    elif op == Operation.DIVISION:
+        return division(a, b)
+    else:
+        raise ValueError("Invalid operation")
+    
+if __name__ == "__main__":
+    num1 = float(input("Insert first number: "))
+    num2 = float(input("Insert second number: "))
+    
+    print(f"SUM: {operation(num1, num2, Operation.SUM)}")
+    print(f"Difference: {operation(num1, num2, Operation.DIFFERENCE)}")
+    print(f"Multiplication: {operation(num1, num2, Operation.MULTIPLICATION)}")
+    print(f"Division: {operation(num1, num2, Operation.DIVISION)}")
